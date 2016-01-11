99b273ddb4dee043ea477d9b348f956550c2b1f8
Simon Schmidt <simon.schmidt@htp-tel.de>
2016-01-11 13:13:30 +0100

    tfif

---
+++ src/lwstack/tfif.c
@@ -0,0 +1,32 @@
+/*
+ *
+ * Copyright 2016 Simon Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "tfif.h"
+
+proc_result_t tfif_input(odp_packet_t pkt,void* args) ODP_HOT_CODE;
+
+proc_result_t tfif_input(odp_packet_t pkt,void* args){
+	proc_result_t result;
+	struct TCPAll filter = *((struct TCPAll*)args);
+	odph_tcphdr_t* hdr = odp_packet_l3_ptr(pkt);
+	result.packet_handle = pkt;
+	result.next_action = ((hdr->doffset_flags&filter.mask)==filter.data)?NA_CONTINUE:NA_DROP;
+	return result;
+}
+
+
---
+++ src/lwstack/tfif.h
@@ -0,0 +1,43 @@
+/*
+ *
+ * Copyright 2016 Simon Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#pragma once
+
+#include "pipeline.h"
+#include <odp/helper/tcp.h>
+
+/*
+ * tfif.h:
+ * TCP Flags Input Filter
+ */
+
+typedef unsigned int bit;
+
+/*
+ * (odph_tcphdr_t*)->doffset_flags;
+ */
+struct TCPAll{
+	uint16be_t mask;
+	uint16be_t data;
+}
+
+/* args must be `struct TCPAll' */
+proc_result_t tfif_input(odp_packet_t pkt,void* args);
+
+
+
